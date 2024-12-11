f9727396ffe0bcbb607a9a4143b394075af94344
wayneowl <waynensnv@icloud.com>
2024-12-11 15:43:52 +0800

    BASE MODEL COMPLETED
    This is the first project of my JS learning.

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
-// intialize the count as 0
-// listen for clicks on the increment button
-// increment the count variable when the button is clicked
-// change the count-el in the HTML to reflect the new count
-
 let countEL = document.getElementById("count-el");
-let saveEl = document.getElementById("save-el");
+let records = document.getElementById("records");
 let count = 0;
 
 function increment() {
@@ -14,15 +9,7 @@ function increment() {
 
 function save() {
 	let latestCount = count + " - ";
-	saveEl.textContent += latestCount;
+	records.textContent += latestCount;
 	count = 0;
 	countEL.textContent = count;
 }
-
-// Welcome
-
-// let welcomeEL = document.getElementById("welcome-el");
-// let userName = "Wayne";
-// let greeting = "Welcome back ";
-// welcomeEL.innerText = greeting + userName;
-// welcomeEL.innerText += " 👋";
